09a68c554fb77cd714eb04a236738815b31a41b3
Kjell4 <ilias.bekturgan@mail.ru>
2024-09-27 16:45:59 +0500

    Models added

---
+++ project/courses/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from courses.models import Course, Tag, Prerequesite, Learning, Video
+
+class TagAdmin(admin.TabularInline):
+    model = Tag
+
+class LearningAdmin(admin.TabularInline):
+    model = Learning
+
+class PrerequesiteAdmin(admin.TabularInline):
+    model = Prerequesite
+
+class VideoAdmin(admin.TabularInline):
+    model = Video
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TagAdmin, LearningAdmin, PrerequesiteAdmin, VideoAdmin]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Video)
+
+
