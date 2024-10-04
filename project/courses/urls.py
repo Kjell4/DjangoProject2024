7ec0ed0e9c232eb59704b8c78691b4d9cc176e66
Kumar <k_oskenbay@kbtu.kz>
2024-10-04 19:37:45 +0500

    Added courses, filters some tags

--- project/courses/urls.py
+++ project/courses/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import HttpResponse
 from courses.views import home
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', home, name='home')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
