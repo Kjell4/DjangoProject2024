3cd1e6b91d7addf3da172b7b6b9fa713398f587e
Bibolat2005 <nur650biko@gmail.com>
2024-10-07 23:22:15 +0500

    registration and signup added

--- project/courses/urls.py
+++ project/courses/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import HttpResponse
-from courses.views import home, coursePage
+from courses.views import home, coursePage, SignupView, login
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
     path('', home, name = 'home'),
+    path('signup/', SignupView.as_view(), name = 'signup'),
+    path('login/', login, name = 'login'),
     path('course/<str:slug>', coursePage, name = 'coursepage'),
 ] 
 
---
+++ project/courses/views/auth.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from courses.models import Course , Video
+from django.shortcuts import HttpResponse
+from courses.forms import RegistrationForm
+from django.views import View
+
+class SignupView(View):
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, 
+        template_name="courses/signup.html", context = {'form':form})
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if(form.is_valid()):
+            user =form.save()
+            if(user is not None):
+                return redirect('login')
+        return render(request, 
+        template_name="courses/signup.html", context = {'form':form})
+
+
+
+
+    
+
+
+def login(request):
+    return render(request, 
+    template_name="courses/login.html")
