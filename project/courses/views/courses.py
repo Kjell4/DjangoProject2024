090ec21720af7495e71d153dc8b53e31ca8ba1c9
Bibolat2005 <nur650biko@gmail.com>
2024-10-18 09:07:00 +0500

    Exam works correct

--- project/courses/views/courses.py
+++ project/courses/views/courses.py
@@ -1,36 +1,36 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from courses.models import Course, Video
 from user_payment.models import UserPayment
 from django.urls import reverse
+from exam.models import TestResult 
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def coursePage(request, slug):
-    course = Course.objects.get(slug=slug)
+    course = get_object_or_404(Course, slug=slug)
     serial_number = request.GET.get('lecture')
 
+    # Получаем все видео для курса
     videos = course.video_set.all().order_by("serial_number")
     
     if serial_number is None:
         serial_number = 1
 
-    try:
-        video = Video.objects.get(serial_number=serial_number, course=course)
-    except Video.DoesNotExist:
-        return redirect('course_page', slug=course.slug)
+    video = get_object_or_404(Video, serial_number=serial_number, course=course)
+
+
+    test_result = TestResult.objects.filter(user=request.user, video=video).first()
 
     user_payment = UserPayment.objects.filter(app_user=request.user, course=course, payment_bool=True).exists()
     
     if not user_payment and not video.is_preview:
         return redirect('product_page', slug=course.slug)
 
-    if serial_number and int(serial_number) == videos.count():
-        return redirect('take_test', video_id=video.id)
-
     context = {
         "course": course,
         "video": video,
         'videos': videos,
         'product_page_url': reverse('product_page', kwargs={'slug': course.slug}),
+        'test_result': test_result, 
     }
     return render(request, template_name="courses/course_page.html", context=context)
--- project/exam/models.py
+++ project/exam/models.py
@@ -18,8 +18,19 @@ class Question(models.Model):
 class UserAnswer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    selected_option = models.IntegerField()  # Хранит номер выбранного варианта (1, 2, 3 или 4)
+    selected_option = models.IntegerField()
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user.username} - {self.question.question_text}'
+    
+class TestResult(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    total_questions = models.IntegerField()
+    passed = models.BooleanField(default=False) 
+    date_taken = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f'{self.user.username} - {self.video.title} - {self.score}/{self.total_questions}'
