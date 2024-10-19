12a03676cad0c3f3147ea14aed7b174c39afeab8
Kjell4 <ilias.bekturgan@mail.ru>
2024-10-20 00:04:59 +0500

    Courses added, payment templates improved, sign up improved, nav bars added

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
-    'user_profile',
     'exam',
     'user_payment.apps.UserPaymentConfig',
     'user_profile',
